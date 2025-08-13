68e8a6b61dea7c85be46f1751d5f3509a2c25fdf
Gabriel Christopher <bielgrecy@gmail.com>
2025-08-13 20:25:48 -0300

    Create exercicio1.kt
    Exercício1 Aula1 de programação mobile

---
+++ exercicio1.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val pipoca = 18
+    val cachorro_quente = 7
+    var clientes = 10
+    
+    /* Alguns clientes desistiram de ficar na fila  */
+    
+    clientes = 8
+    println("Tem $clientes clientes, $cachorro_quente cachorros quentes e $pipoca pipocas")
+}
