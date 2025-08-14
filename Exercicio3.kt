33aa6d59b04d3b25a1f39cceefd27afdbc352e70
Gabriel Christopher <bielgrecy@gmail.com>
2025-08-13 21:32:52 -0300

    Create Exercicio3.kt

---
+++ Exercicio3.kt
@@ -0,0 +1,14 @@
+fun main(){
+	var cliente = 10
+	cliente += 7
+    println("O número de clientes sombado com 7 é $cliente\n")
+    
+    cliente -= 3
+    println("O número de clientes subtraido por 3 é $cliente\n")
+    
+    cliente *= 2
+    println("O número de clientes multiplicados por 2 é $cliente\n")
+    
+    cliente /= 3
+    println("O número de clientes dividido por 3 é $cliente")
+}
